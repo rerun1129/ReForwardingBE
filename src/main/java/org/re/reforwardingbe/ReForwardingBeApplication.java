95ad981f64e4dfcbc8dd4b4d10d82e75f96f57ee
KoYunSeok <a01021719359@gmail.com>
2025-09-26 15:02:15 +0900

    Initial commit

---
+++ src/main/java/org/re/reforwardingbe/ReForwardingBeApplication.java
@@ -0,0 +1,13 @@
+package org.re.reforwardingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReForwardingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReForwardingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/re/reforwardingbe/ReForwardingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.re.reforwardingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReForwardingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
